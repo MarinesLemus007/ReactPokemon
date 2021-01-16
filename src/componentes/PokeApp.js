6aa649c27f326fb1ecc57000b133f144d6c8a195
MarinesLemus007 <marineslemus@gmail.com>
2021-01-16 16:07:05 -0300

    creando componentes

---
+++ src/componentes/PokeApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PokeTarjeta from "./PokeTarjeta";
+
+class PokeApp extends Component {
+  render() {
+    return (
+        <PokeTarjeta/>
+    );
+  }
+}
+export default PokeApp;
---
+++ src/componentes/PokeTarjeta.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PokeTarjeta = (props) =>{
+    return (
+        <div>
+            <h3>Personaje</h3>
+            <p>Tipo</p>
+        </div>
+    );
+}
+export default PokeTarjeta;

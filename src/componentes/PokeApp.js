00adb9f8e5353034ff9dd751f90ee23d36581c32
MarinesLemus007 <marineslemus@gmail.com>
2021-01-16 16:20:39 -0300

    agregando axios

--- src/componentes/PokeApp.js
+++ src/componentes/PokeApp.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import PokeTarjeta from "./PokeTarjeta";
 
 class PokeApp extends Component {
+
+componentDidMount(){
+    
+}
+
   render() {
     return (
         <PokeTarjeta/>
